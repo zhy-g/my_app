41347cf1f0391eaca828f5cf764dfc22cc4d1a02
zhy-g <1926963751@qq.com>
2019-11-06 15:27:01 +0800

    new app

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>Hello World</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/pages/home/iconfonts/header.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class="">返回</span>
+        </div>
+        <div class="header-input">内容</div>
+        <div class="header-right">城市</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader',
+    components: {
+      HomeHeader
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    height: .88rem
+    background: #00bcd4
+    .header-left
+      float left
+    .header-input
+      background #ffffff
+    .header-right
+      float right
+</style>
