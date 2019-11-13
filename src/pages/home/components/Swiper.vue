6ac7292aaf024b180015a9e2c3f8cd26c865e26b
zhy-g <192696963751@qq.com>
2019-11-13 15:55:55 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -7,7 +7,9 @@
     <swiper-slide>
     <img class='swiper-img' src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'/>
     </swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
+    <swiper-slide>
+      <img class="swiper-img" src="https://img1.qunarzz.com/sight/p0/1802/40/40b2075640718d7a3.img.png_600x330_11cd3f65.png" alt="">
+    </swiper-slide>
     <swiper-slide>I'm Slide 4</swiper-slide>
     <!-- Optional controls -->
     <div class="swiper-pagination"  slot="pagination"></div>
@@ -27,7 +29,7 @@ export default {
 }
 </script>
 
-<style lang='stylus' scoped>
+<style lang='stylus'>
  .swiper-img
    width: 100%
 </style>
