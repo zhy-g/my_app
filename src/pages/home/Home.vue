274e59e5f6ad228971dc7fa5772e23ec4fe14e06
zhy-g <192696963751@qq.com>
2019-11-13 15:47:44 +0800

    commit file

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
        <home-header></home-header>
+       <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper'
+
 export default {
     name: 'Home',
     components: {
