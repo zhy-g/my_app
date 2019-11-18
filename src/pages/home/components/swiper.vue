356be369fc6c4db96614617d372b8dc489e609f3
zhy-g <19263751@qq.com>
2019-11-18 09:46:10 +0800

    finish

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -1,43 +1,60 @@
 <template>
     <div class="wrapper">
-     <swiper :options="swiperOption" >
-    <!-- slides -->
-    <swiper-slide>
-    <img class='swiper-img' src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/c8762210baff5ade1a3b2fe0ab914992.png_750x200_9bbb2f51.png' />
-    </swiper-slide>
-    <swiper-slide>
-    <img class='swiper-img' src='http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'/>
-    </swiper-slide>
-    <swiper-slide>
-      <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/e7eac5a5e99e6517c61b953c0fece63b.jpg_750x200_4b701cf8.jpg">
-    </swiper-slide>
-    <swiper-slide>
-      <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/2b3a9445cc37885eb13e0402baef9f5e.png_750x200_24df2b41.png" alt="">
-    </swiper-slide>
-    <!-- Optional controls -->
-   
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper> </div>
+        <!-- 在swiper外层添加一个类名为wrapper的div标签 -->
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for='item of swiperList' :key='item.id'>
+              <img :src='item.imgUrl' class="swiper-img">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <!-- 利用v-for来循环遍历swiperList数组，添加绑定key值来实现轮播的顺序 -->
+          
+            <div class="swiper-pagination" slot="pagination"></div>
+          </swiper>
+    </div>
 </template>
-
 
 <script>
 export default {
   name: 'HomeSwiper',
   data() {
     return {
-      swiperOption:{}
+      swiperOption:{
+        pagination: '.swiper-pagination',
+        // 添加pagination选项并赋值.swiper-pagination来实现轮播图上圆点选项
+        loop:true
+        // 添加loop选项来实现轮播图的循环
+      },
+      swiperList:[{
+        id: '001',
+        imgUrl:' http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/c8762210baff5ade1a3b2fe0ab914992.png_750x200_9bbb2f51.png'
+      },{
+        id: '002',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/1d8faa9f4d49b77ca1e61e2b478260f7.jpg_750x200_25cf76e2.jpg'
+      },{
+        id:"003",
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/e7eac5a5e99e6517c61b953c0fece63b.jpg_750x200_4b701cf8.jpg'
+      }, {
+        id: '004',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201911/2b3a9445cc37885eb13e0402baef9f5e.png_750x200_24df2b41.png'
+      }]
+      // 添加swiperList数组来实现管理轮播图播放的顺序和图片地址
     }
   }
  
 }
 </script>
 
-<style lang='stylus'> 
+<style lang='stylus' scoped> 
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+    // 实现.wrapper对.swiper-pagination-bullet-active的样式穿透
   .wrapper
     overflow hidden
+    width 100%
+    height 0
     padding-bottom 26.66%
-    width: 100%
+    // .wrapper实现按比例占位
     .swiper-img
      width: 100%
 </style>
